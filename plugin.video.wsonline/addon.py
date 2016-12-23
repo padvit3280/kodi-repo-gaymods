bb8706c318af63833c931e9420f20ba1c474e1ce
moedje <r00trox>
2016-12-23 04:58:56 +0000

    Trying to fix repo to work correctly in Kodi for installing and updating addons instead of having to manually install via zip file. If this works it will be Version 1.0 plugin.video.hubgay has worked and updated in testing.

--- plugin.video.wsonline/addon.py
+++ plugin.video.wsonline/addon.py
@@ -227,8 +227,8 @@ def episode(name, url):
 def play(url):
     plugin.set_view_mode(0)
     plugurl = 'plugin://plugin.video.hubgay/playmovie/{0}'.format(urllib.quote_plus(url))
-    plugin.play_video(plugurl)
-    return plugin.end_of_directory()
+    xbmc.executebuiltin('RunPlugin({0})'.format(plugurl))
+    #return plugin.end_of_directory()
     #xbmc.executebuiltin('RunPlugin(plugin://plugin.video.hubgay/playmovie/%s)' % urllib.quote_plus(url))
     #return [plugin.set_resolved_url(url)]
 
